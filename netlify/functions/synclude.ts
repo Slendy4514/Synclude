391bf4665067c9a1d3e6dcc11eb26a8ae6af21a3
Slendy4514 <52763557+Slendy4514@users.noreply.github.com>
2022-10-08 21:44:59 -0300

    Netlify Functions

---
+++ netlify/functions/synclude.ts
@@ -0,0 +1,29 @@
+import { Handler } from '@netlify/functions';
+import * as dotenv from 'dotenv'
+dotenv.config()
+import Messenger from "../../src/lib/Messenger";
+import Discord from "../../src/lib/Discord";
+
+const handler : Handler = async () => {
+    const discord = new Discord(process.env.TOKEN as string)
+    const messenger = new Messenger(Messenger.createCookies(
+    process.env.XS as string,
+    process.env.C_USER as string
+))
+
+messenger.on('message', (msg : any) => discord.send(msg.nick, msg.profilePic, msg.text, msg.img))
+const prevUser : string = ''
+discord.client.on('messageCreate', async (msg) =>{
+    if(!msg.author.bot){
+        prevUser === msg.author.username || await messenger.send(`> ${msg.author.username} from Discordlude:`)
+        await messenger.send(`${msg.content}`)
+    }
+})
+discord.init()
+return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Synclude Iniciado" }),
+  };
+}
+
+export {handler}
